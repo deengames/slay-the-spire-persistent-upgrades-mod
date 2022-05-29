f740264332a9f6cfb20c446ce4045b618279be73
nightblade9 <nightbladecodes@gmail.com>
2022-05-29 18:59:11 -0400

    First cut (untested) of code.

--- src/main/java/com/deengames/slaythespire/persistentupgrades/PersistentUpgradesMod.java
+++ src/main/java/com/deengames/slaythespire/persistentupgrades/PersistentUpgradesMod.java
@@ -1,8 +1,14 @@
 package com.deengames.slaythespire.easymode;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
 import basemod.BaseMod;
 import basemod.interfaces.ISubscriber;
-import basemod.interfaces.OnStartBattleSubscriber;
+import basemod.interfaces.PostDeathSubscriber;
+import basemod.interfaces.StartGameSubscriber;
 
 import com.evacipated.cardcrawl.modthespire.lib.SpireInitializer;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
@@ -10,7 +16,10 @@ import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.rooms.AbstractRoom;
 
 @SpireInitializer
-public class PersistentUpgradesMod implements ISubscriber {
+/// Stores the number of games played in a text file; uses that to determine amount of upgrades.
+public class PersistentUpgradesMod implements ISubscriber, StartGameSubscriber, PostDeathSubscriber {
+
+	private final String DATA_FILE_NAME = "PersistentUpgradesMod.dat";
 
 	public static void initialize() {
         new PersistentUpgradesMod();
@@ -18,5 +27,45 @@ public class PersistentUpgradesMod implements ISubscriber {
 	
 	public PersistentUpgradesMod() {
 		BaseMod.subscribe(this);
+	}
+
+	public void receiveStartGame() {
+		int numGames = getNumGames();
+		// TODO: apply 2n max HP, 1n strength, 1n metalicize
+	}
+
+	public void receivePostDeath() {
+		int numGames = getNumGames();
+		numGames += 1;
+
+		setNumGames(numGames);
+	}
+
+	private int getNumGames() {
+		if (!Files.exists(Paths.get(DATA_FILE_NAME)))
+		{
+			return 0;
+		}
+
+		try {
+			List<String> lines = Files.readAllLines(Paths.get(DATA_FILE_NAME));
+			final String contents = String.join("\n", lines);
+			final int numGames = Integer.parseInt(contents);
+			return numGames;
+		} catch (IOException e)
+		{
+			return 0;
+		}
+	}
+
+	private void setNumGames(int numGames) {
+		try {
+			final String contents = String.valueOf(numGames);
+			final byte[] asByte = contents.getBytes();
+			Files.write(Paths.get(DATA_FILE_NAME), asByte);
+		} catch (IOException e)
+		{
+
+		}
 	}
 }
